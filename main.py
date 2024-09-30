7a83e1db0c15b053ac71bb4f5586107db615efe5
shresti00 <shrestisubahar@gmail.com>
2024-09-30 19:43:15 +0530

    Added emojis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from discord.ext import commands
 
 from discord.utils import setup_logging
 
@@ -11,11 +12,25 @@ load_dotenv(dotenv_path)
 
 token = os.environ.get("TOKEN")
 
+bot=bot = commands.Bot(command_prefix='!')
 
 async def main() -> None:
     setup_logging()
-    async with Bot() as bot:
-        await bot.start(token=token, reconnect=True)
+    await bot.start(token=token, reconnect=True)
+        
+async def choose_emoji(ctx):
+
+    message = await ctx.send("Choose an emoji:\n😂 - Laughing\n🤣 - Rolling on the floor laughing\n😭 - Crying")
+
+    await message.add_reaction("😂")
+    await message.add_reaction("🤣")
+    await message.add_reaction("😭")
+
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) in ["😂", "🤣", "😭"]
+
+    reaction, user = await bot.wait_for("reaction_add", check=check)
+    await ctx.send(f'You chose: {reaction.emoji}')
 
 
 if __name__ == "__main__":
