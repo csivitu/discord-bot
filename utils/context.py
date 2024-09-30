e4cd7ef0e5ef742d02cfccbd242b9f80c786b51d
PDGamerSG <pallab4586das@gmail.com>
2024-09-30 20:16:35 +0530

    emoji command fixed issue #8

--- utils/context.py
+++ utils/context.py
@@ -4,14 +4,4 @@ from discord import Embed
 
 
 class Context(OldContext):
-    async def reply(
-        self,
-        content: Optional[str] = None,
-        *args: Any,
-        embed: Optional[Embed] = None,
-        create_embed: Optional[bool] = False,
-        **kwargs: Any,
-    ) -> Coroutine:
-        if content.count(":") > 1:
-            return await super().reply(content.replace(":", ""))
-        return await super().reply("Haha Nice Try")
+    pass
